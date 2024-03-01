eb348de385cf89189869ba0322e4d52d3996ae00
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-01 15:18:21 -0500

    Modified common.hpp to include commonly used standard library headers and introduce utility macros for optional types

--- src/include/common.hpp
+++ src/include/common.hpp
@@ -1,6 +1,26 @@
+/// *==============================================================*
+///  common.hpp
+///
+///  Includes commonly used standard library headers and defines
+///  some utility macros for optional types.
+/// *==============================================================*
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
-// TODO
+#include <cstdint>
+#include <cstdbool>
+#include <cstdlib>
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include <variant>
+
+#include <vector>
+#include <unordered_map>
+
+#include <optional>
+#define some(value) std::optional(value);
+#define none(_)     std::nullopt;
 
 #endif /* COMMON_HPP */
