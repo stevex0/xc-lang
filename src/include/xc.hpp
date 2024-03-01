364e4804968f8f1a25b1367376e54a1f42896e24
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-01 15:25:27 -0500

    Added xc.hpp

---
+++ src/include/xc.hpp
@@ -0,0 +1,17 @@
+/// *==============================================================*
+///  xc.hpp
+///
+///  Contains declarations for the XC language.
+/// *==============================================================*
+#ifndef XC_HPP
+#define XC_HPP
+
+#include "common.hpp"
+
+namespace XC {
+
+    void compile(const std::string target);
+
+}
+
+#endif /* XC_HPP */
