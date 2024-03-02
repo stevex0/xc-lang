24a9a3c495527f30e72298efa14a29cde535e2ac
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-02 13:49:59 -0500

    Added sourcefile.hpp

---
+++ src/include/sourcefile.hpp
@@ -0,0 +1,23 @@
+/// *==============================================================*
+///  sourcefile.hpp
+///
+///  Contains the declaration of the SourceFile struct.
+/// *==============================================================*
+#ifndef SOURCEFILE_HPP
+#define SOURCEFILE_HPP
+
+#include "common.hpp"
+
+namespace XC {
+
+    struct SourceFile {
+    public:
+        std::string filename;
+        std::vector<std::string> content;
+
+        static std::optional<std::unique_ptr<SourceFile>> loadContent(const std::string filepath);
+    };
+
+}
+
+#endif /* SOURCEFILE_HPP */
