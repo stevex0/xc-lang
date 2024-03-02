4f03e6893288b0e0ae19ae0b63c5832dc45f5694
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-02 13:48:38 -0500

    Modified main function to handle command line arguments and call the compile() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/// *==============================================================*
+///  main.cpp
+/// *==============================================================*
+#include "include/xc.hpp"
+
 int main(int argc, char** argv) {
-    return 0;
+    if (argc <= 1) {
+        std::cerr << "usage:\n\txc [TARGET]" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    XC::compile(std::string(argv[1]));
+
+    return EXIT_SUCCESS;
 }
