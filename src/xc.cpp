e2cf41cdd4388c8bedd402c195d405ac0506acc5
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-02 14:00:17 -0500

    Modified the compile() function to create a Module and load content from a file

--- src/xc.cpp
+++ src/xc.cpp
@@ -1,9 +1,22 @@
+/// *==============================================================*
+///  xc.cpp
+/// *==============================================================*
 #include "include/xc.hpp"
 
 namespace XC {
 
     void compile(const std::string target) {
-        // TODO
+        std::unique_ptr<Module> module = std::make_unique<Module>();
+
+        module->source = SourceFile::loadContent(target).value_or(nullptr);
+        if (module->source == nullptr) {
+            exit(EXIT_FAILURE);
+        }
+
+        // TODO: Tokenization
+        // TODO: Syntax Analysis
+        // TODO: Semantic Analysis
+        // TODO: Code Generation 
     }
 
 }
