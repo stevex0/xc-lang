3419a1422cf5a0b1b256da880b3f72207afe430c
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-08 12:55:51 -0500

    Modified to include `using namespace` for improved code clarity and simplicity

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 /// *==============================================================*
 #include "include/xc.hpp"
 
+using namespace XC;
+
 int main(int argc, char** argv) {
     if (argc <= 1) {
         std::cerr << "usage:\n\txc [TARGET]" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    XC::compile(std::string(argv[1]));
+    compile(std::string(argv[1]));
 
     return EXIT_SUCCESS;
 }
