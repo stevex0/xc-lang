2891ee0c4fbd6be4cb2c77444ab5aa4cc4252dcc
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-08 12:53:42 -0500

    Tokenization is now integrated into the compilation process

--- src/xc.cpp
+++ src/xc.cpp
@@ -2,21 +2,25 @@
 ///  xc.cpp
 /// *==============================================================*
 #include "include/xc.hpp"
+#include "include/sourcefile.hpp"
+#include "include/tokenizer.hpp"
 
-namespace XC {
+using namespace XC;
 
-    void compile(const std::string target) {
-        std::unique_ptr<Module> module = std::make_unique<Module>();
+void XC::compile(const std::string target) {
+    std::unique_ptr<Module> module = std::make_unique<Module>();
 
-        module->source = SourceFile::loadContent(target).value_or(nullptr);
-        if (module->source == nullptr) {
-            exit(EXIT_FAILURE);
-        }
-
-        // TODO: Tokenization
-        // TODO: Syntax Analysis
-        // TODO: Semantic Analysis
-        // TODO: Code Generation 
+    module->source = SourceFile::loadContent(target).value_or(nullptr);
+    if (module->source == nullptr) {
+        exit(EXIT_FAILURE);
     }
 
+    module->tokens = Tokenizer(module).extractTokenStream().value_or(nullptr);
+    if (module->tokens == nullptr) {
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: Syntax Analysis
+    // TODO: Semantic Analysis
+    // TODO: Code Generation 
 }
