da770d6be1184f2847c36910cc01d9621048d11b
SteveX0101 <86204023+SteveX0101@users.noreply.github.com>
2024-03-08 12:46:51 -0500

    Modified the Module struct to include the TokenStream struct

--- src/include/xc.hpp
+++ src/include/xc.hpp
@@ -8,12 +8,14 @@
 
 #include "common.hpp"
 #include "sourcefile.hpp"
+#include "token.hpp"
 
 namespace XC {
 
     struct Module {
     public:
         std::unique_ptr<SourceFile> source;
+        std::unique_ptr<TokenStream> tokens;
     };
 
     void compile(const std::string target);
